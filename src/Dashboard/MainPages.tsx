b6f86088cbb636eba452fccd6805d719feb618ae
umeh emmanuel <eumeh3882@gmail.com>
2023-09-02 07:44:14 -0700

    emmanuel

--- src/Dashboard/MainPages.tsx
+++ src/Dashboard/MainPages.tsx
@@ -3,8 +3,13 @@ import { FiShoppingBag } from "react-icons/fi";
 import { useProduct } from "../hook/Product";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart } from "../Global/GlobalState";
+import Detailed from "./Detailed";
+import { useState } from "react";
 
 const MainPages = () => {
+
+  const [state, setState] = useState<boolean>(false);
+  const [propsState, setPropsState] = useState<{}>({} as any);
   const dispatch = useDispatch();
   const { data, isLoading } = useProduct();
   const cart = useSelector((state: any) => state.cart);
@@ -29,7 +34,7 @@ const MainPages = () => {
         <div className="flex justify-center ml-4  flex-wrap">
           {data?.map((props: any) => (
             <div className="w-[250px] h-[300px] bg-red-600 m-2 mb-[100px] border rounded-md relative" key={props.id}>
-              <div className="w-full h-[300px]">
+              <div className="w-full h-[300px] relative mr-2">
                 <img
                   src={props.image}
                   alt=""
@@ -37,7 +42,12 @@ const MainPages = () => {
                 />
                 <div className="w-full justify-center flex absolute bottom-0 pb-3 opacity-0 hover:opacity-100 hover:bg-[rgba(0,0,0,0.2)] transition-all duration-500 h-[300px] items-end">
                   <div className="w-[30px] h-[30px] rounded-[50%] bg-white  flex justify-center items-center hover:cursor-pointer hover:bg-[#3e3e3e] shadow-md hover:text-white duration-300 transition-all ">
-                    <MdOutlineVisibility />
+                    <MdOutlineVisibility 
+                       onClick={() => {
+                        setPropsState(props);
+                        setState(true);
+                      }}
+                    />
                   </div>
                   <div
                     className="w-[30px] h-[30px] rounded-[50%] bg-white ml-3  flex justify-center items-center hover:cursor-pointer hover:bg-[#3e3e3e] hover:text-white duration-300 transition-all shadow-md"
@@ -69,6 +79,14 @@ const MainPages = () => {
           ))}
         </div>
       )}
+     {
+      state && (
+        <div className="fixed top-0 left-0 duration-300 transition-all">
+        <Detailed props={propsState} setState={setState}/>
+      </div>
+      )
+     }
+    
     </div>
   );
 };
